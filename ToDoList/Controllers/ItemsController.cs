785e317d2f92e76085ed4cdd0d2b5d69f0f6ba1b
Noel Kirkland <noelkirkland@gmail.com>
2020-07-23 12:06:55 -0700

    add a bunch of stuff
    Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com>

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
+
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
+    {
+      Category category = Category.Find(categoryId);
+      return View(category);
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+  }
+}
